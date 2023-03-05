c916e88f113e7e67715bc05cdd3a7d7d4b5604ef
Roman <romandzindzyura@gmail.com>
2023-03-06 01:34:47 +0200

    fix bugs

--- client/src/components/module/GeneredeBoard/GederadeCard/GeneradeCard.jsx
+++ client/src/components/module/GeneredeBoard/GederadeCard/GeneradeCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
-import { TaskInformation } from "./GeneradeBoard.style";
+import { TaskInformation, CardName } from "./GeneradeBoard.style";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import moment from "moment";
 import { useState, useRef } from "react";
@@ -14,11 +14,11 @@ const GeneradeCard = ({ item, index, deleteCard, updateCard }) => {
   const now = moment();
   let diff;
   let formattedDiff;
-  if(item.updated_at){
+  if (item.updated_at) {
     date1 = moment(item.updated_at);
     diff = moment.duration(now.diff(date1));
     formattedDiff = diff.humanize();
-  }else{
+  } else {
     date1 = moment(item.created_at);
     diff = moment.duration(now.diff(date1));
     formattedDiff = diff.humanize();
@@ -30,7 +30,7 @@ const GeneradeCard = ({ item, index, deleteCard, updateCard }) => {
       updateCard(id, taskRef.current.textContent);
       const newDiff = moment.duration(now.diff(new Date()));
       formattedDiff = newDiff.humanize();
-      setNewDiff(formattedDiff + 'ago');
+      setNewDiff(formattedDiff + " ago");
     }
   };
 
@@ -44,15 +44,17 @@ const GeneradeCard = ({ item, index, deleteCard, updateCard }) => {
         >
           <TaskInformation>
             <div>
-                  <div
-                  contentEditable={true}
-                  onBlur={() => handleBlur(item.id)}
-                  suppressContentEditableWarning={true}
-                  ref={taskRef}
-                >
-                  {item.Task}
-                </div>
-              <p style={{fontSize: 10}}>{newDiff ? newDiff : formattedDiff + ' ago'}</p>
+              <CardName
+                contentEditable={true}
+                onBlur={() => handleBlur(item.id)}
+                suppressContentEditableWarning={true}
+                ref={taskRef}
+              >
+                {item.Task}
+              </CardName>
+              <p style={{ fontSize: 10 }}>
+                {newDiff ? newDiff : formattedDiff + " ago"}
+              </p>
             </div>
             <button onClick={() => deleteCard(item.id)}>
               <DeleteOutlineIcon />
